89d6ba1e010918bf306a1846d0a6dfd0770548e0
adamaconguli <waltson2003@gmail.com>
2023-08-31 23:20:38 +0800

    added Calendar page, theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './layouts/Layout'
+import Calendar from './pages/Calendar'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Layout />} />
+        <Route path='/' element={<Layout />}>
+          <Route path='calendar' element={<Calendar />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,41 @@
 import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Sidebar() {
   const [fold, setFold] = useState(false)
+  const [mode, setMode] = useState(true)
 
-  const toggle = () => {
+  const toggleFold = () => {
     setFold((prevFold) => !prevFold)
-    console.log('toggle')
+  }
+
+  const toggleMode = () => {
+    setMode((prevMode) => !prevMode)
+    document.querySelector('body').classList.toggle('Dark')
   }
 
   return (
     <div className={`Sidebar ${fold ? 'fold' : ''}`}>
       <div className='Sidebar--title'>
         <i className='bx bx-task'></i>
-        <h1>Task Manager</h1>
+        <span>Task Manager</span>
+        <i className='bx bx-chevron-left toggle' onClick={toggleFold}></i>
       </div>
-      <i className='bx bx-chevron-left toggle' onClick={toggle}></i>
-      <div className='Sidebar--mode'>
-        {/* <i class='bx bx-moon'></i>
-        <i class='bx bx-sun'></i> */}
+      <div className='Sidebar--menu'>
+        <NavLink to='calendar' className='Sidebar--label'>
+          <i className='bx bxs-calendar'></i>
+          <span>Calendar</span>
+        </NavLink>
+        <div className='Sidebar--mode Sidebar--label' onClick={toggleMode}>
+          <div className='mode--icon'>
+            <i className='bx bx-moon'></i>
+            <i className='bx bx-sun'></i>
+          </div>
+          <span>{mode ? 'Dark' : 'Light'} Mode</span>
+          <div className='mode--switch'>
+            <div className='switch'></div>
+          </div>
+        </div>
       </div>
     </div>
   )
