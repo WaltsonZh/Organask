3e791441aad15c1cedefba0bd7f7df6dfaf68e95
adamaconguli <waltson2003@gmail.com>
2023-09-06 22:58:52 +0800

    added return to current month

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -10,14 +10,14 @@ export default function Calendar() {
   })
   const monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-  const [monthlyCalendar, setMonthlyCalendar] = useState(getMonthCalendar(date))
+  const [monthlyCalendar, setMonthlyCalendar] = useState([])
   const monthlyCalendarEls = monthlyCalendar.map((week) => {
     let key = 0
     return (
       <div key={monthlyCalendar.indexOf(week)} className='monthly--week'>
         {week.map((day) => {
           return (
-            <div key={key++} className='monthly--day'>
+            <div key={key++} className={`monthly--day ${date.day === day ? 'today' : ''}`}>
               {day ? day : ''}
             </div>
           )
@@ -27,10 +27,21 @@ export default function Calendar() {
   })
 
   useEffect(() => {
+    if (date.year === today.getFullYear() && date.month === today.getMonth()) {
+      setDate((prevDate) => ({
+        ...prevDate,
+        day: today.getDate(),
+      }))
+    } else {
+      setDate((prevDate) => ({
+        ...prevDate,
+        day: 1,
+      }))
+    }
     setMonthlyCalendar(getMonthCalendar(date))
-  }, [date])
+  }, [date.year, date.month])
 
-  const handleClick = (e) => {
+  const navigate = (e) => {
     const name = e.target.name
     if (name === 'lastYear') {
       setDate((prevDate) => ({
@@ -46,28 +57,39 @@ export default function Calendar() {
       setDate((prevDate) => ({
         ...prevDate,
         year: prevDate.month === 0 ? prevDate.year - 1 : prevDate.year,
-        month: prevDate.month === 0 ? 11 : prevDate.month - 1
+        month: prevDate.month === 0 ? 11 : prevDate.month - 1,
       }))
     } else {
       setDate((prevDate) => ({
         ...prevDate,
         year: prevDate.year + Math.floor(prevDate.month / 11),
-        month: (prevDate.month + 1) % 12
+        month: (prevDate.month + 1) % 12,
       }))
     }
+  }
+
+  const returnToday = () => {
+    setDate({
+      day: today.getDate(),
+      month: today.getMonth(),
+      year: today.getFullYear(),
+    })
   }
 
   return (
     <div className='Calendar Page'>
       <h1>Calendar</h1>
       <div className='calendar--header'>
-        <button className='bx bx-chevrons-left' name='lastYear' onClick={handleClick}></button>
-        <button className='bx bx-chevron-left' name='lastMonth' onClick={handleClick}></button>
-        <h2>
-          {monthName[date.month]}, {date.year}
-        </h2>
-        <button className='bx bx-chevron-right' name='nextMonth' onClick={handleClick}></button>
-        <button className='bx bx-chevrons-right' name='nextYear' onClick={handleClick}></button>
+        <div className='navigation'>
+          <button className='bx bx-chevrons-left' name='lastYear' onClick={navigate}></button>
+          <button className='bx bx-chevron-left' name='lastMonth' onClick={navigate}></button>
+          <h2>
+            {monthName[date.month]}, {date.year}
+          </h2>
+          <button className='bx bx-chevron-right' name='nextMonth' onClick={navigate}></button>
+          <button className='bx bx-chevrons-right' name='nextYear' onClick={navigate}></button>
+        </div>
+        <button className='bx bx-navigation return' onClick={returnToday}></button>
       </div>
       <div className='calendar--grid'>{monthlyCalendarEls}</div>
     </div>
